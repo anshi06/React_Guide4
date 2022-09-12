333951a2d96d71de915d1169cdd04b697174f69b
anshi06 <araghya6768@gmail.com>
2022-09-12 10:22:25 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const storedUserLoggedInInformation = localStorage.getItem('LoggedIn')
-  if(storedUserLoggedInInformation === 1){
-    setIsLoggedIn(true);
-  }
 
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('LoggedIn');
 
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  //Now this function inside useEffect is executed by react and
+  // it is executed after every component re-evaluation and when dependencies are changed.
+  //Here denpendecy array is empty so when app starts then this function runs,
+  // after that there are no dependency then it will not run again.
+  //If we use this function w/o using use Effect then it will go into infinite loop.
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
@@ -21,6 +28,7 @@ function App() {
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('LoggedIn')
     setIsLoggedIn(false);
   };
 
