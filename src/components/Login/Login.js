a778037ffa5c5a5f6105dd5acf6832961a13d8fb
anshi06 <araghya6768@gmail.com>
2022-09-13 09:44:25 +0530

    useReducer working

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useReducer} from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+const emailReducer = (state, action)=>{
+  if(action.type === 'USER_INPUT'){
+    return {value: action.val , isValid: action.val.includes("@")}
+  }
+  if(action.type === 'INPUT_BLUR'){
+    return {value: state.value , isValid: state.value.includes("@")}
+  }
+  return {value: '' , isValid: false} 
+  // return new state which could be a object
+
+}
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState("");
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
+
+  const[emailState, dispatchEmail] = useReducer(emailReducer, {value: '' , isValid: null},);
   //Step 1:
   // useEffect(()=>{
   //   console.log('Effect Running');
@@ -40,21 +53,21 @@ const Login = (props) => {
   // }, [])
 
 
-  useEffect(() => {
-    console.log("checking validity");
-    const identifier = setTimeout(() => {
-      setFormIsValid(
-        enteredEmail.includes("@") && enteredPassword.trim().length > 6
-      );
-    }, 500);
-    return () => {
-      console.log("CLEAN UP");
-      clearTimeout(identifier);
-    };
-    // this is a clean up function we can return in use effect function , this will perform a clean up process before
-    // this function executes for next time. It does not run before the very first side effect function run. it runs before every new
-    //side effect function executes.
-  }, [enteredEmail, enteredPassword]);
+  // useEffect(() => {
+  //   console.log("checking validity");
+  //   const identifier = setTimeout(() => {
+  //     setFormIsValid(
+  //       enteredEmail.includes("@") && enteredPassword.trim().length > 6
+  //     );
+  //   }, 500);
+  //   return () => {
+  //     console.log("CLEAN UP");
+  //     clearTimeout(identifier);
+  //   };
+  //   // this is a clean up function we can return in use effect function , this will perform a clean up process before
+  //   // this function executes for next time. It does not run before the very first side effect function run. it runs before every new
+  //   //side effect function executes.
+  // }, [enteredEmail, enteredPassword]);
 
   //debouncing:- we will not do our work work for every key stroke ..because in complex situation such as in sending http request
   //it can create network traffic and it is also possible that it sends too many unneccesary requests so debouncing is done .
@@ -62,15 +75,23 @@ const Login = (props) => {
   // FOR EVERY KEYSTROKE WE ARE SETTING A TIMER
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+   // setEnteredEmail(event.target.value);
+   dispatchEmail({type: 'USER_INPUT', val: event.target.value})
+    setFormIsValid(
+      event.target.value.includes("@") && enteredPassword.trim().length > 6
+    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+    setFormIsValid(
+      emailState.isValid && event.target.value.trim().length > 6
+    );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes("@"));
+    //setEmailIsValid(emailState.isValid);
+    dispatchEmail({type:'INPUT_BLUR'})
   };
 
   const validatePasswordHandler = () => {
@@ -79,7 +100,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -87,14 +108,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ""
+            emailState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
